fe84992bd4fdf3011829e9f3796e10b4c396ec8c
Ross Soloff <rsoloff@gmail.com>
2015-12-15 15:49:51 -0500

    fix strict

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app         = express();
 
 mongoose.connect(process.env.MONGOLAB_URI||'mongodb://127.0.0.1:27017/rpgApp');
 
-let db = mongoose.connection;
+var db = mongoose.connection;
 db.on('error',console.error.bind(console, 'connection error:'));
 db.once('open',  callback=>console.log('mongoose connected'));
 
